55b54fd90bd9a1ed5054ae52c572b588fd49727a
David Heidt <heidave@amazon.de>
2018-07-31 13:48:28 +0200

    webservice with tiome

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func helloWorld(w http.ResponseWriter, r *http.Request) {
+	t := time.Now()
+	fmt.Fprintf(w, t.String())
+}
+
+func main() {
+	http.HandleFunc("/", helloWorld)
+	http.ListenAndServe(":8080", nil)
+}
